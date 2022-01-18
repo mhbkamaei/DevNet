9f1a495025f91bfca5ba7dead6a199c1fd9b317f
hbb <mh.b.kamaei@gmail.com>
2022-01-18 13:38:58 +0330

    f&j

--- first.py
+++ first.py
@@ -1,1 +1,17 @@
-print("hello world")
+from netmiko import ConnectHandler
+from netmiko import SSHDetect
+
+device = {
+    "device_type": "cisco_ios",
+    "host": "10.10.10.10",
+    "username": "cisco",
+    "password": "cisco"
+}
+
+ssh = ConnectHandler(**device)
+print(ssh.find_prompt())
+#print(con.send_command('show version'))
+ssh.enable()
+ssh.send_config_set('hostname switch2')
+print(ssh.find_prompt())
+ssh.disconnect()
